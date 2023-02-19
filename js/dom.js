6def3442e24c851df43dc60ce16ec11201ca602f
Naim Ali <naimalinaim1@gmail.com>
2023-02-19 23:06:42 +0600

    recap dom

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,22 @@
 document.getElementById('apply-bg').addEventListener('click', function () {
-    const friends = document.getElementsByClassName('friend-name');
+    const friends = document.getElementsByClassName('friend');
     for (const friend of friends) {
-        console.log(friend);
+        friend.style.background = 'lightblue';
     }
+});
+
+document.getElementById('friend-center').addEventListener('click', function () {
+    const thirdFriend = document.getElementById('third-friend');
+    thirdFriend.style.textAlign = 'center';
+});
+
+document.getElementById('add-friends').addEventListener('click', function () {
+    const container = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.classList.add('friend')
+    friend.innerHTML = `
+        <h3 class="friend-name">new Friend</h3>
+        <p>something new added</p>
+    `;
+    container.appendChild(friend);
 })
